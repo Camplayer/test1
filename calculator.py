6ff58130008a1e265d3d0a3bc80304cc6a910085
Camplayer <camplayer@126.com>
2017-03-19 14:15:16 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ c = a + b
 
 # commit from owner
 
-# 03/19/2017 added by Camplayer in bosplayer's folder
+# 03/19/2017 added by Bosplayer in bosplayer's folder
 def addTwoNumbers(a, b):
 	pass
 	return a + b
